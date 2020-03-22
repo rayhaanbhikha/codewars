3cfe85bf493926883369889d48ef3006fa526ee9
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-03-22 22:00:01 +0000

    v2 zero_to_infinity

--- zero_to_infinity/main.go
+++ zero_to_infinity/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	Going(6)
 }
@@ -8,12 +10,11 @@ func Going(n int) float64 {
 
 	currentSum, currentBase := 1.0, 1.0
 
-	for i := 0; i < n-1; i++ {
-		newN := float64(n - i)
-		currentBase *= newN
-		appendVal := 1 / currentBase
-		currentSum += appendVal
+	for i := 0.0; i < n-1; i++ {
+		currentBase *= float64(n - i)
+		currentSum += (1 / currentBase)
 	}
 	v := int(currentSum * 10e5)
+	fmt.Println(v)
 	return float64(v) / 10e5
 }
